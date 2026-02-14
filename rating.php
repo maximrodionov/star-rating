b954ab3d494863f738c9e9526dd66b138c69b2e4
Максим Родионов <25633048+maximrodionov@users.noreply.github.com>
2026-02-15 00:15:11 +0300

    rating.php

--- rating.php
+++ rating.php
@@ -1,6 +1,6 @@
 <?php
 $rating = 3; // известный рейтинг 
 
-for ($i = 1; $i <= 4; $i++) {
+for ($i = 2; $i <= 4; $i++) { // set $i to 2 to show empty stars for lowest rating ☆☆☆☆ (1/4)
     echo ($i <= $rating) ? "★" : "☆";
 }
